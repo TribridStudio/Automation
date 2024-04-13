4638b23241d424ed63d5f235834f9c94b1cbba59
Jaimee Nye <nyejaimee92@gmail.com>
2024-04-13 11:41:23 -0400

    Update timestamp.py

--- timestamp.py
+++ timestamp.py
@@ -5,14 +5,8 @@ import datetime
 
 # Parse the Final Cut Pro XML file
 tree = ET.parse('./info.fcpxml')
-
-# Get the root element of the XML tree
 root = tree.getroot()
-
-# Define the namespace used in the XML file
 ns = {'fcpxml': 'http://www.apple.com/fcpxml-1.0/'}
-
-# Find all marker elements in the XML tree
 markerParents = root.findall('.//marker/..', ns)
 
 def parseFCPTimeSeconds (timeString):
@@ -23,12 +17,12 @@ def parseFCPTimeSeconds (timeString):
         val = vals[0]/vals[1]
     return val
 
-# Loop through the marker elements and extract the start attribute value
+
 for parents in markerParents:
     offset = parseFCPTimeSeconds(parents.get('offset'))
     start = parseFCPTimeSeconds(parents.get('start'))
 
-    # print("Parent offset: " + str(offset))
+
     markers = parents.findall(".//marker")
     for marker in markers:
         s = parseFCPTimeSeconds(marker.get('start'))
